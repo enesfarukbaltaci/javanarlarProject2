9b3d46f28b11a4e16b7096f9b1d1af951b49c76f
Enes Faruk Baltacı <efarukbaltaci@gmail.com>
2022-04-12 00:31:10 +0200

    12.04.2022

---
+++ src/javanarlarGitHubOgreniyor.java
@@ -0,0 +1,7 @@
+public class javanarlarGitHubOgreniyor {
+    public static void main(String[] args) {
+
+        System.out.println("Javanarlara selam olsun, vakit pull vaktidir HUCUUM!");
+
+    }
+}
