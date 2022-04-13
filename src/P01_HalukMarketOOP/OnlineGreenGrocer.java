e5295c02af4d2979fda417f0a7c083597e2ef2e9
javafadu <javafadu@gmail.com>
2022-04-14 00:01:10 +0300

    14 apr

---
+++ src/P01_HalukMarketOOP/OnlineGreenGrocer.java
@@ -0,0 +1,13 @@
+package P01_HalukMarketOOP;
+
+
+public class OnlineGreenGrocer {
+
+    public static void main(String[] args) {
+
+        System.out.println("******* WELCOME TO ONLINE GREENGROCER *******");
+
+        Transactions.menu();
+
+    }
+}
